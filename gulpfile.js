04fe38419673994f6d75b3fbd07af0b73fa197a2
“cam” <camille.farge@hotmail.com>
2019-05-13 15:10:10 +0200

    debut du projet (sans bootstrap)

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,56 @@
 const gulp  = require('gulp');
-const {lastRun} = require ('gulp');
+const {lastRun, series} = require ('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require("gulp-autoprefixer");
-const del = require('del')
+const rename = require('gulp-rename');
+const del = require('del');
+const spritesmith = require('gulp.spritesmith');
+const gulpif = require("gulp-if");
 
 
 
-
-function style(){
+function style (){
    //where is my scss file
    return gulp.src('./scss/**/style.scss', {sourcemaps: true}, {since: lastRun(sass)})
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-
-
+   // adapté à tous les browser
+   .pipe(
+    autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    //renome css en .min
+    .pipe(
+        rename({
+          suffix: '.min'
+        })
+      )
    //destination
    .pipe(gulp.dest('./css', {sourcemaps: '.' }))
    .pipe(browserSync.stream());
 }
 
-function watch(){
+
+function clean() {
+    return del('css');
+   }
+
+function sprites() {
+    return  gulp.src('img/*.png')
+                .pipe(spritesmith({
+                    imgName: 'sprite.png',
+                    cssName: 'sprite.scss',
+                    imgPath: '../img/sprites/sprite.png'
+                }))
+                //return spriteData.pipe(gulp.dest('sprites/'));
+                .pipe(gulpif('*.png', gulp.dest('img/sprites')))
+                .pipe(gulpif('*.scss', gulp.dest('scss/modules')));
+};
+
+
+
+function watch() {
    browserSync.init({
        server: {
            baseDir: './'
@@ -30,11 +61,13 @@ function watch(){
    gulp.watch('./**/*.html').on('change', browserSync.reload);
 }
 
-//exports.style = style;
-//exports.watch = watch;
+
 
 module.exports = {
-    watch
+    //default: series(clean,  ), 
+    watcher: series(clean,sprites, watch),
+    sprites: sprites,
+    style:style,
 }
 
 
