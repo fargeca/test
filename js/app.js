a0d5e79598a353548818f2cef6b899ff7b712d13
“cam” <camille.farge@hotmail.com>
2019-05-31 15:47:25 +0200

    menu xs, new colors

--- js/app.js
+++ js/app.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
 $('.sidebar__nav-item').on('click', function() {
     $(this).toggleClass('clicked');
 });
+
+
+$('.sidebarCollapse').on('click', function () {
+    $('.sidebar-wrapper').toggleClass('sidebar--active');
 });
+
+$('.searchbarCollapse').on('click', function () {
+    $('.searchbar').toggleClass('searchbar--active');
+});
+
+
+});
+
